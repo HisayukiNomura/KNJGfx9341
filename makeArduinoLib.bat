e53822064e3fbbe976ce467f536278b6ee5d4d50
HisayukiNomura <hnomura@fa2.so-net.ne.jp>
2025-09-04 09:15:40 +0900

    fixed Arduino Library Generator - Example sketch (Text.ino): font name and rotation enum - Replaced include paths in some *.cpp files to match Arduino include conventions - Commented out #pragma region (some Japanese characters cause compilation errors)

--- makeArduinoLib.bat
+++ makeArduinoLib.bat
@@ -22,15 +22,43 @@ rmdir /q /s KNJGfx9341_ARD >> outlog.txt 2>&1
 mkdir KNJGfx9341_ARD >> outlog.txt 2>&1
 
 echo  "---------- coping file to working folder ----------" >> outlog.txt 2>&1
-xcopy /e /i /h /y  lib-9341\* KNJGfx9341_ARD\ >> outlog.txt 2>&1
+rem xcopy /e /i /h /y  lib-9341\* KNJGfx9341_ARD\ >> outlog.txt 2>&1
+robocopy lib-9341 KNJGfx9341_ARD /E /COPYALL /XD ForMPY >> outlog.txt 2>&1
 
 cd KNJGfx9341_ARD
 for /r %%d in (*.cpp) do move "%%d" . >> ..\outlog.txt 2>&1
 for /r %%d in (*.c) do move "%%d" . >> ..\outlog.txt 2>&1
 
-pushd  Adafruit_GFX_Library\Fonts
-
 echo "---------- Editing ASCII font files --------" >> outlog.txt 2>&1
+echo $replacements = @{ > pswork.ps1
+echo     '#include "defines.h"'          = '#include "misc/defines.h"' >> pswork.ps1
+echo     '#include "Adafruit_ILI9341.h"' = '#include "Adafruit_ILI9341/Adafruit_ILI9341.h"' >> pswork.ps1
+echo } >> pswork.ps1
+echo $Regexreplacements = @{ >> pswork.ps1
+echo     '^\s*#pragma region' = '// #pragma region ' >> pswork.ps1
+echo     '^\s*#pragma endregion' = '// #pragma endregion' >> pswork.ps1
+echo } >> pswork.ps1
+
+echo. >> pswork.ps1
+echo Get-ChildItem *.cpp ^| ForEach-Object { >> pswork.ps1
+echo     $content = Get-Content $_.FullName -Encoding UTF8 >> pswork.ps1
+echo     foreach ($pattern in $replacements.Keys) { >> pswork.ps1
+echo         $content = $content -replace [regex]::Escape($pattern), $replacements[$pattern] >> pswork.ps1
+echo     } >> pswork.ps1
+
+echo     foreach ($pattern in $Regexreplacements.Keys) { >> pswork.ps1
+echo         $content = $content -replace $pattern, $Regexreplacements[$pattern] >> pswork.ps1
+echo     } >> pswork.ps1
+echo     $content ^| Set-Content $_.FullName -Encoding UTF8 >> pswork.ps1
+echo } >> pswork.ps1
+
+
+pause
+powershell -ExecutionPolicy Bypass -File pswork.ps1 >> ..\outlog.txt 2>&1
+del pswork.ps1
+
+
+pushd  Adafruit_GFX_Library\Fonts
 echo Get-ChildItem *.h ^| ForEach-Object { (Get-Content $_.FullName) -replace '#include ^<Adafruit_GFX.h^>', '#include "../Adafruit_GFX.h"' ^| Set-Content $_.FullName } > pswork1.ps1
 powershell -ExecutionPolicy Bypass -File pswork1.ps1 >> ..\outlog.txt 2>&1
 del pswork1.ps1
@@ -55,7 +83,7 @@ cd ../..
 echo  "---------- making zip archive and copy it to USERPROFILE% ----------" >> outlog.txt 2>&1
 7z a KNJGfx9341_ARD_ONLY.zip KNJGfx9341_ARD -xr!*.bak  >> outlog.txt 2>&1
 rmdir /q /s KNJGfx9341_ARD  >> outlog.txt 2>&1
-rem copy  KNJGfx9341_ARD.zip %USERPROFILE%  >> outlog.txt 2>&1
+copy  KNJGfx9341_ARD_ONLY.zip %USERPROFILE%  >> outlog.txt 2>&1
 rem del KNJGfx9341_ARD.zip  >> outlog.txt 2>&1
 echo successfully created KNJGfx9341_ARD_ONLY.zip
 echo   *If an error occurs, the execution log was saved in outlog.txt.
